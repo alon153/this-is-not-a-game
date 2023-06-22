ec12cb984c30fa4a53ace1fb57d6787a7c21fb04
GalShaffir <80719879+GalShap@users.noreply.github.com>
2023-06-22 17:46:50 +0300

    added scrolling road

--- Assets/Scripts/GameMode/Boats/BoatsRoad.cs
+++ Assets/Scripts/GameMode/Boats/BoatsRoad.cs
@@ -1,24 +1,40 @@
+using System;
 using UnityEngine;
 
-public class BoatsRoad : MonoBehaviour
+namespace GameMode.Boats
 {
-  #region Serialized Fields
-  
-  #endregion
-  #region Non-Serialized Fields
-  
-  #endregion
-  #region Properties
-  
-  #endregion
-  #region Function Events
-  
-  #endregion
-  #region Public Methods
-  
-  #endregion
-  #region Private Methods
-  
-  #endregion
+  public class BoatsRoad : MonoBehaviour
+  {
+     #region Serialized Fields
+
+     [SerializeField] private float roadSpeed = 2f;
+
+     #endregion
+
+     #region Non-Serialized Fields
+
+     private Material _rendererMaterial;
+
+     private float _offset;
+     
+     private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+     #endregion
+
+     #region MonoBehaviour
+
+     private void Start()
+     {
+         _rendererMaterial = GetComponent<SpriteRenderer>().material;
+     }
+
+     private void Update()
+     {
+         _offset -= (Time.deltaTime * roadSpeed) / 10f;
+         _rendererMaterial.SetTextureOffset(MainTex, new Vector2(0, _offset));
+     }
+
+     #endregion
+  }
 }
 
